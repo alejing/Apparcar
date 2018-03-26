7af02043a98a2b12068a2c69c54ed9e149cbff2b
Alejandro Franco <alejing@gmail.com>
2018-03-25 23:19:58 -0500

    Se crea el splash screen y se garage el logo del launcher de la app

---
+++ app/src/main/java/com/proyecto/android/apparcar/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.proyecto.android.apparcar;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int DURACION_SPLASH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION_SPLASH);
+
+
+    }
+}
